bd4c2e585ae75f45716d14de3818db6f9a0b259e
Nivas Muthu <nivasmuthu452@gmail.com>
2020-08-04 19:10:55 +0530

    ChangeNotifier implementaion. Seperated count for isDone into seperate function. Moved updateTodo to todo.dart using setters. Implemented setters for changing title, accentcolor. Functions to add, delete todo Functions to update isDoneCount, TotalTasks

--- lib/model/todocollection.dart
+++ lib/model/todocollection.dart
@@ -2,24 +2,18 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:todo/model/todo.dart';
 
-class TodoCollection {
+class TodoCollection extends ChangeNotifier {
   String _title;
   List<Todo> _todo;
   int _isDoneCount = 0;
   int _totalTasks;
-  ColorSwatch _colorSwatch = Colors.blue;
+  ColorSwatch _accentColor = Colors.blue;
 
   TodoCollection({@required String title, List<Todo> todo})
       : assert(title != null),
         _title = title,
         _todo = todo ?? [],
-        _totalTasks = todo.length {
-    todo.forEach((element) {
-      if (element.isDone == true) {
-        ++_isDoneCount;
-      }
-    });
-  }
+        _totalTasks = todo.length;
 
   String get title => _title;
 
@@ -27,15 +21,47 @@ class TodoCollection {
 
   int get totalTasks => _totalTasks;
 
-  ColorSwatch get colorSwatch => _colorSwatch;
+  ColorSwatch get accentColor => _accentColor;
 
-  Todo getTodo(int index) => _todo[index];
+  List<Todo> get todo => _todo;
 
-  void updateTodo(int index, {String title, bool value}) {
-    if (title == null) {
-      _todo[index].changeIsDone = value;
-    } else if (value == null) {
-      _todo[index].changeTitle = title;
-    }
+  set changeTitle(String title) {
+    if (title == _title) return;
+    _title = title;
+    notifyListeners();
+  }
+
+  set changeAccentColor(ColorSwatch accentColor) {
+    if (accentColor == _accentColor) return;
+    _accentColor = accentColor;
+    notifyListeners();
+  }
+
+  void updateTotalTasks() {
+    _totalTasks = todo.length;
+  }
+
+  void updateDoneCount() {
+    int temp = 0;
+    todo.forEach((element) {
+      if (element.isDone == true) {
+        temp++;
+      }
+    });
+    _isDoneCount = temp;
+    notifyListeners();
+  }
+
+  void addTodo(String title) {
+    Todo temp = Todo(title: 'title');
+    _todo.add(temp);
+    updateTotalTasks();
+    notifyListeners();
+  }
+
+  void deleteTodo(int index) {
+    _todo.removeAt(index);
+    updateTotalTasks();
+    notifyListeners();
   }
 }
