307c73ab1db03166aa469b5dafa05a1791d5b11c
Nivas Muthu <nivasmuthu452@gmail.com>
2020-08-04 19:40:32 +0530

    Defined Consumer to use the TodoRepository Provider

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo/model/todocollection.dart';
+import 'package:todo/repo/todorepo.dart';
 import 'package:todo/screens/todopage.dart';
 import 'package:todo/widgets/progressbar.dart';
 
@@ -28,59 +29,61 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: ClipOval(
-                    child: CircleAvatar(),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 20.0,
-                  ),
-                  child: Text(
-                    'Hello, Nivas',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30.0,
+      body: Consumer<TodoRepository>(
+        builder: (context, todoRepository, _) => Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: ClipOval(
+                      child: CircleAvatar(),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text(
-                    'You have 3 taks to do today',
-                    style: TextStyle(
-                      color: Colors.white,
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20.0,
+                    ),
+                    child: Text(
+                      'Hello, Nivas',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30.0,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 40.0),
-            child: Container(
-              height: 380,
-              child: PageView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: _todoCollectionList.length,
-                itemBuilder: (context, index) => ChangeNotifierProvider.value(
-                  value: _todoCollectionList[index],
-                  child: TodoCollectionCardBuilder(),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text(
+                      'You have 3 taks to do today',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40.0),
+              child: Container(
+                height: 380,
+                child: PageView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: _todoCollectionList.length,
+                  itemBuilder: (context, index) => ChangeNotifierProvider.value(
+                    value: _todoCollectionList[index],
+                    child: TodoCollectionCardBuilder(),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
