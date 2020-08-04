e033e2b368e00dfad9b311bfe987fb2b4273f4fb
Nivas Muthu <nivasmuthu452@gmail.com>
2020-08-04 19:11:25 +0530

    Provider implementation

--- lib/screens/todopage.dart
+++ lib/screens/todopage.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo/model/todo.dart';
 import 'package:todo/model/todocollection.dart';
 import 'package:todo/widgets/progressbar.dart';
 
-class TodoPage extends StatefulWidget {
+class TodoPage extends StatelessWidget {
   final TodoCollection _todoCollection;
 
   TodoPage({@required TodoCollection todoCollection})
@@ -10,118 +12,138 @@ class TodoPage extends StatefulWidget {
         _todoCollection = todoCollection;
 
   @override
-  _TodoPageState createState() =>
-      _TodoPageState(todoCollection: _todoCollection);
-}
-
-class _TodoPageState extends State<TodoPage> {
-  final TodoCollection _todoCollection;
-
-  _TodoPageState({@required TodoCollection todoCollection})
-      : assert(todoCollection != null),
-        _todoCollection = todoCollection;
-
-  @override
   Widget build(BuildContext context) {
-    return Hero(
-      tag: widget._todoCollection.title,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          actions: [
-            IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        backgroundColor: Colors.white,
-        body: ListView(
-          children: [
-            Padding(
-              padding:
-                  const EdgeInsets.only(top: 40.0, left: 40.0, right: 40.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    '12 Tasks',
-                    style: TextStyle(
-                      color: Colors.grey[600],
-                      // fontSize: 30.0,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Text(
-                    _todoCollection.title,
-                    style: TextStyle(
-                      color: Colors.grey[600],
-                      fontSize: 30.0,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 30.0),
-                    child: ProgressBar(todoCollection: _todoCollection),
-                  ),
-                  Text(
-                    'Todo',
-                    style: TextStyle(
-                      color: Colors.grey[600],
-                      // fontSize: 30.0,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                ],
+    return Consumer<TodoCollection>(
+      builder: (context, todoCollection, _) => Hero(
+        tag: _todoCollection.title,
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            actions: [
+              IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {},
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40.0),
-              child: ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
-                itemCount: _todoCollection.totalTasks,
-                itemBuilder: (context, index) => ListTile(
-                  contentPadding: const EdgeInsets.all(0.0),
-                  title: Text(_todoCollection.getTodo(index).title),
-                  leading: SizedBox(
-                    width: 24,
-                    height: 24,
-                    child: Checkbox(
-                      value: _todoCollection.getTodo(index).isDone,
-                      onChanged: (bool value) => setState(() =>
-                          _todoCollection.updateTodo(index, value: value)),
+            ],
+          ),
+          backgroundColor: Colors.white,
+          body: ListView(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.only(top: 40.0, left: 40.0, right: 40.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      '12 Tasks',
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        // fontSize: 30.0,
+                      ),
                     ),
-                  ),
-                  trailing: Visibility(
-                    visible: _todoCollection.getTodo(index).isDone,
-                    child: IconButton(
-                      icon: Icon(Icons.delete_outline),
-                      onPressed: () {},
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Text(
+                      _todoCollection.title,
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        fontSize: 30.0,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 30.0),
+                      child: ProgressBar(
+                          totalTask: todoCollection.totalTasks,
+                          isDoneCount: todoCollection.isDoneCount),
+                    ),
+                    Text(
+                      'Todo',
+                      style: TextStyle(
+                        color: Colors.grey[600],
+                        // fontSize: 30.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: _todoCollection.totalTasks,
+                  itemBuilder: (context, index) => MultiProvider(
+                    providers: [
+                      ChangeNotifierProvider.value(
+                          value: _todoCollection.todo.elementAt(index)),
+                      ChangeNotifierProvider.value(value: _todoCollection),
+                    ],
+                    child: CustomListTile(
+                      index: index,
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-        floatingActionButton: Padding(
-          padding: const EdgeInsets.only(bottom: 20.0),
-          child: RaisedButton(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Icon(
-                Icons.add,
-                color: Colors.white,
-                size: 30.0,
+            ],
+          ),
+          floatingActionButton: Padding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            child: RaisedButton(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Icon(
+                  Icons.add,
+                  color: Colors.white,
+                  size: 30.0,
+                ),
               ),
+              color: _todoCollection.colorSwatch,
+              onPressed: () {},
+              shape: CircleBorder(),
             ),
-            color: _todoCollection.colorSwatch,
-            onPressed: () {},
-            shape: CircleBorder(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomListTile extends StatelessWidget {
+  final int _index;
+
+  CustomListTile({int index}) : _index = index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Todo>(
+      builder: (context, todo, _) => ListTile(
+        contentPadding: const EdgeInsets.all(0.0),
+        title: Text(todo.title),
+        leading: SizedBox(
+          width: 24,
+          height: 24,
+          child: Checkbox(
+            value: todo.isDone,
+            onChanged: (bool value) {
+              todo.changeIsDone = value;
+              Provider.of<TodoCollection>(context, listen: false)
+                  .updateDoneCount();
+            },
+          ),
+        ),
+        trailing: Visibility(
+          visible: todo.isDone,
+          child: IconButton(
+            icon: Icon(Icons.delete_outline),
+            onPressed: () {
+              Provider.of<TodoCollection>(context, listen: false)
+                  .deleteTodo(_index);
+            },
           ),
         ),
       ),
