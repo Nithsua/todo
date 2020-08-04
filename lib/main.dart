1ebed0237b7050a94f114923e76554fda515388b
Nivas Muthu <nivasmuthu452@gmail.com>
2020-08-04 19:40:14 +0530

    Created Provider to use TodoRepository

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo/model/todo.dart';
 import 'package:todo/model/todocollection.dart';
+import 'package:todo/repo/todorepo.dart';
 import 'package:todo/screens/homepage.dart';
 
 List<TodoCollection> todoCollectionList = [
@@ -38,8 +40,12 @@ class MyApp extends StatelessWidget {
         ),
         scaffoldBackgroundColor: Colors.blue,
       ),
-      home: HomePage(
-        todoCollectionList: todoCollectionList,
+      home: ChangeNotifierProvider<TodoRepository>(
+        create: (context) =>
+            TodoRepository(todoCollectionList: todoCollectionList),
+        child: HomePage(
+          todoCollectionList: todoCollectionList,
+        ),
       ),
     );
   }
