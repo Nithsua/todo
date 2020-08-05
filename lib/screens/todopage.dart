123c1c257afe9a876db4126e6578a3e12b52fa5c
Nivas Muthu <nivasmuthu452@gmail.com>
2020-08-05 10:41:08 +0530

    Passing down TodoRepository Provider down the tree To update the count

--- lib/screens/todopage.dart
+++ lib/screens/todopage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo/model/todo.dart';
 import 'package:todo/model/todocollection.dart';
+import 'package:todo/repo/todorepo.dart';
 import 'package:todo/widgets/progressbar.dart';
 
 class TodoPage extends StatelessWidget {
@@ -13,6 +14,8 @@ class TodoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var todoRepository = Provider.of<TodoRepository>(context);
+
     return Consumer<TodoCollection>(
       builder: (context, todoCollection, _) => Hero(
         tag: _todoCollection.title,
@@ -36,7 +39,7 @@ class TodoPage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      '12 Tasks',
+                      '${todoCollection.totalTasks} Tasks',
                       style: TextStyle(
                         color: Colors.grey[600],
                         // fontSize: 30.0,
@@ -79,11 +82,12 @@ class TodoPage extends StatelessWidget {
                   itemCount: _todoCollection.totalTasks,
                   itemBuilder: (context, index) => MultiProvider(
                     providers: [
+                      ChangeNotifierProvider.value(value: _todoCollection),
                       ChangeNotifierProvider.value(
                           value: _todoCollection.todo.elementAt(index)),
-                      ChangeNotifierProvider.value(value: _todoCollection),
+                      ChangeNotifierProvider.value(value: todoRepository),
                     ],
-                    child: CustomListTile(
+                    builder: (context, _) => CustomListTile(
                       index: index,
                     ),
                   ),
@@ -133,18 +137,17 @@ class CustomListTile extends StatelessWidget {
               todo.changeIsDone = value;
               Provider.of<TodoCollection>(context, listen: false)
                   .updateDoneCount();
+              Provider.of<TodoRepository>(context, listen: false).updateCount();
             },
           ),
         ),
-        trailing: Visibility(
-          visible: todo.isDone,
-          child: IconButton(
-            icon: Icon(Icons.delete_outline),
-            onPressed: () {
-              Provider.of<TodoCollection>(context, listen: false)
-                  .deleteTodo(_index);
-            },
-          ),
+        trailing: IconButton(
+          icon: Icon(Icons.delete_outline),
+          onPressed: () {
+            Provider.of<TodoCollection>(context, listen: false)
+                .deleteTodo(_index);
+            Provider.of<TodoRepository>(context, listen: false).updateCount();
+          },
         ),
       ),
     );
