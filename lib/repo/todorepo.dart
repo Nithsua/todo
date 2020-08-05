3efa5aefbfc6032f93195b71b6f2b1854213b51c
Nivas Muthu <nivasmuthu452@gmail.com>
2020-08-05 10:39:46 +0530

    Added total count for tasks and completed tasks in todoRepo

--- lib/repo/todorepo.dart
+++ lib/repo/todorepo.dart
@@ -3,9 +3,16 @@ import 'package:todo/model/todocollection.dart';
 
 class TodoRepository extends ChangeNotifier {
   final List<TodoCollection> _todoCollectionList;
+  int totalTasks = 0;
+  int isDoneCount = 0;
 
   TodoRepository({@required List<TodoCollection> todoCollectionList})
-      : _todoCollectionList = todoCollectionList;
+      : _todoCollectionList = todoCollectionList {
+    todoCollectionList.forEach((element) {
+      totalTasks += element.totalTasks;
+      isDoneCount += element.isDoneCount;
+    });
+  }
 
   void addCollection(TodoCollection todoCollection) {
     _todoCollectionList.add(todoCollection);
@@ -14,5 +21,18 @@ class TodoRepository extends ChangeNotifier {
 
   void deleteCollection(int index) {
     _todoCollectionList.removeAt(index);
+    notifyListeners();
   }
+
+  void updateCount() {
+    totalTasks = 0;
+    isDoneCount = 0;
+    todoCollectionList.forEach((element) {
+      totalTasks += element.totalTasks;
+      isDoneCount += element.isDoneCount;
+    });
+    notifyListeners();
+  }
+
+  List<TodoCollection> get todoCollectionList => _todoCollectionList;
 }
