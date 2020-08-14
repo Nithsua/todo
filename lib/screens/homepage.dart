92b450023f1c1f4e6566de4639c5556bbcaa7197
Nivas Muthu <nivasmuthu452@gmail.com>
2020-08-14 12:58:27 +0530

    Replaced Consumer() with FutureBuilder()

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo/localstore/localstore.dart';
 import 'package:todo/repo/todorepo.dart';
 import 'package:todo/widgets/todocollectioncard.dart';
 
@@ -21,66 +22,72 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<TodoRepository>(
-        builder: (context, todoRepository, _) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: ClipOval(
-                        child: CircleAvatar(),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 20.0,
-                      ),
-                      child: Text(
-                        'Hello, Nivas',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 30.0,
+      body: FutureBuilder(
+        future: localStore.getLocalData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            TodoRepository todoRepository = snapshot.data;
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: ClipOval(
+                          child: CircleAvatar(),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Text(
-                        'You have ${todoRepository.totalTasks - todoRepository.isDoneCount} tasks to do today',
-                        style: TextStyle(
-                          color: Colors.white,
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 20.0,
+                        ),
+                        child: Text(
+                          'Hello, Nivas',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30.0,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Text(
+                          'You have ${todoRepository.totalTasks - todoRepository.isDoneCount} tasks to do today',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 40.0),
-                child: Container(
-                  height: 380,
-                  child: PageView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: todoRepository.todoCollectionList.length,
-                    itemBuilder: (context, index) => MultiProvider(
-                      providers: [
-                        ChangeNotifierProvider.value(
-                            value: todoRepository.todoCollectionList[index]),
-                        ChangeNotifierProvider.value(value: todoRepository)
-                      ],
-                      builder: (context, _) => TodoCollectionCard(),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 40.0),
+                  child: Container(
+                    height: 380,
+                    child: PageView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: todoRepository.todoCollectionList.length,
+                      itemBuilder: (context, index) => MultiProvider(
+                        providers: [
+                          ChangeNotifierProvider.value(
+                              value: todoRepository.todoCollectionList[index]),
+                          ChangeNotifierProvider.value(value: todoRepository)
+                        ],
+                        builder: (context, _) => TodoCollectionCard(),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          );
+              ],
+            );
+          } else {
+            return CircularProgressIndicator();
+          }
         },
       ),
     );
