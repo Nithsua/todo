18552ef45b39dca66de3ac7d16aa1d152bc8256b
Nivas Muthu <nivasmuthu452@gmail.com>
2020-08-14 14:30:10 +0530

    Implemented fromJson() and toJson()

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -4,6 +4,19 @@ class User extends ChangeNotifier {
   String _fullName;
   String _displayName;
 
+  User({@required String fullName})
+      : _fullName = fullName ?? '',
+        _displayName = (fullName ?? '').split(' ')[0];
+
+  User.fromJson(Map<String, dynamic> json) {
+    _fullName = json['fullName'];
+    _displayName = json['displayName'];
+  }
+
+  Map<String, dynamic> toJson() => {
+        "fullName": _fullName,
+        "displayName": _displayName,
+      };
   String get fullName => _fullName;
 
   String get displayName => _displayName;
