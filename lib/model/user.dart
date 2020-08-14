d3d96f83ec7f1fd64e1b89545ad8a4b0fad4de2b
Nivas Muthu <nivasmuthu452@gmail.com>
2020-08-14 14:29:33 +0530

    Defined getters and setters

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
 class User extends ChangeNotifier {
-  String fullName;
-  String displayName;
+  String _fullName;
+  String _displayName;
+
+  String get fullName => _fullName;
+
+  String get displayName => _displayName;
+
+  set setFullName(String fullName) => _fullName = fullName;
+
+  set setDisplayName(String displayName) => _displayName = displayName;
 }
